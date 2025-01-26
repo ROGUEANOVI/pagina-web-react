027f70e0ccd19830051dc1378fae51593e175b26
rogueanovi <ovidioromero66@gmail.com>
2025-01-26 11:18:04 -0500

    deploy-4

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://rogueanovi.github.io//pagina-web-react/",
+  base: "https://ROGUEANOVI.github.io/pagina-web-react/",
 });
